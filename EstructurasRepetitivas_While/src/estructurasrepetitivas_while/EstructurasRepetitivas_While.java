a63eb167a2c161788733fc40152aea9012fdad47
ufopress <ufopress@hotmail.com>
2025-03-15 12:24:54 -0300

    Se carga capítulo Bucle While controlado por contador

--- EstructurasRepetitivas_While/src/estructurasrepetitivas_while/EstructurasRepetitivas_While.java
+++ EstructurasRepetitivas_While/src/estructurasrepetitivas_while/EstructurasRepetitivas_While.java
@@ -1,9 +1,20 @@
 package estructurasrepetitivas_while;
 
+import java.util.Random;
+
 public class EstructurasRepetitivas_While {
 
     public static void main(String[] args) {
-        
+
+        //Ejemplo bucle controlado por contador
+        int valorRandom = 0, vuelta = 0;
+        Random rand = new Random();
+
+        while (vuelta < 10) {
+            valorRandom = rand.nextInt(50);
+            System.out.println("Valor random:" + valorRandom + " en la vuelta: " + vuelta);
+            vuelta++;
+        }
     }
-    
+
 }
