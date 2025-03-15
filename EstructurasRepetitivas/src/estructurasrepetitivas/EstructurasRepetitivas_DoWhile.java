d26b9f15d9b41ae7a9870aa0964d4c75e845276c
ufopress <ufopress@hotmail.com>
2025-03-15 12:19:01 -0300

    Se agrega capítulo Bucle Do-While y empieza con capítulo Bucle While

---
+++ EstructurasRepetitivas/src/estructurasrepetitivas/EstructurasRepetitivas_DoWhile.java
@@ -0,0 +1,22 @@
+package estructurasrepetitivas;
+
+public class EstructurasRepetitivas_DoWhile {
+
+    public static void main(String[] args) {
+        int contador = 1;
+        
+        do{
+            System.out.println("Valores de contador en bucle Do While Nro. 1:" + contador);
+            contador++;
+        }while(contador<10);
+        
+        contador = 20;
+        
+        do{
+            System.out.println("Valores de contador en bucle Do While Nro. 2:" + contador);
+            contador++;
+        }while(contador<10);
+        
+    }
+    
+}
---
+++ EstructurasRepetitivas_While/src/estructurasrepetitivas_while/EstructurasRepetitivas_While.java
@@ -0,0 +1,9 @@
+package estructurasrepetitivas_while;
+
+public class EstructurasRepetitivas_While {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
