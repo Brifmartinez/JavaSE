48eeb2d30a94e3b477d7393e6adab76b077c488d
BrianFernandez <briandanielm@outlook.es>
2025-03-23 13:57:08 -0300

    Se añade contenido de capítulo Arrays (unidireccionales)

---
+++ Arreglos(Arrays)_Unidimensionales/src/arreglos/arrays/_unidimensionales/ArreglosArrays_Unidimensionales.java
@@ -0,0 +1,19 @@
+package arreglos.arrays._unidimensionales;
+
+public class ArreglosArrays_Unidimensionales {
+
+    public static void main(String[] args) {
+        int numeros[] = new int[8];
+        numeros[0] = 35;
+        numeros[1] = 40;
+        numeros[2] = 23;
+        numeros[3] = 11;
+        numeros[4] = 91;
+        numeros[5] = 15;
+        numeros[6] = 6;
+        numeros[7] = 12;
+
+        System.out.println("El número en la quinta posición es: " + numeros[4]);
+    }
+
+}
