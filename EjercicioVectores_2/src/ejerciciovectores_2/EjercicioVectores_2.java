3420a256f4db21325886f52b00a4552f2fb085a9
BrianFernandez <briandanielm@outlook.es>
2025-04-12 01:45:38 -0300

    Se añade ejercicio 2 vectores

---
+++ EjercicioVectores_2/src/ejerciciovectores_2/EjercicioVectores_2.java
@@ -0,0 +1,30 @@
+package ejerciciovectores_2;
+
+import java.util.Scanner;
+
+public class EjercicioVectores_2 {
+
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+        int numeros[] = new int[10];
+        int mayor = Integer.MIN_VALUE, menor=Integer.MAX_VALUE, indice = 0;
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println("Ingrese el valor de la posición : " + (i + 1));
+            numeros[i] = teclado.nextInt();
+        }
+
+        teclado = new Scanner(System.in);
+
+        do {
+            mayor = (numeros[indice] > mayor) ? numeros[indice] : mayor;
+            menor = (numeros[indice] < menor) ? numeros[indice] : menor;
+            indice++;
+        } while (indice < numeros.length);
+        
+        System.out.println("El número de mayor valor ingresado fue: " + mayor);
+        System.out.println("El número de menor valor ingresado fue: " + menor);
+    }
+
+}
