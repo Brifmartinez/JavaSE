cdabd8988e474e9d961f4c66c0580305dce29f34
BrianFernandez <briandanielm@outlook.es>
2025-04-14 23:14:15 -0300

    Se carga comienzo de ejercicio 4_VectoresBidimensionales

---
+++ EjercicioVectores_4/src/ejerciciovectores_4/EjercicioVectores_4.java
@@ -0,0 +1,16 @@
+package ejerciciovectores_4;
+
+public class EjercicioVectores_4 {
+
+    public static void main(String[] args) {
+        
+        /*
+        En una tabla de 4 filas y 3 columnas se guardan las notas de 4 alumnos de secundario.
+        Cada fila corresponde a las notas de cada alumno
+        */
+        
+        
+        
+    }
+    
+}
